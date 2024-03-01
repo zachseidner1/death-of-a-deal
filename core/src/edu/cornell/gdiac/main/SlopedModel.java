adf9e47c67033fa4afab7ccefcc02c9d5aaaea2c
zachseidner1 <zachary.seidner@gmail.com>
2024-02-29 22:38:18 -0500

    Update SlopedModel to extend PolygonObstacle instead of box

--- core/src/edu/cornell/gdiac/main/SlopedModel.java
+++ core/src/edu/cornell/gdiac/main/SlopedModel.java
@@ -2,9 +2,19 @@ package edu.cornell.gdiac.main;
 
 import com.badlogic.gdx.utils.JsonValue;
 import edu.cornell.gdiac.assets.AssetDirectory;
-import edu.cornell.gdiac.physics.obstacle.BoxObstacle;
+import edu.cornell.gdiac.physics.obstacle.PolygonObstacle;
 
-public class SlopedModel extends BoxObstacle {
+public class SlopedModel extends PolygonObstacle {
+
+  public SlopedModel(float[] points) {
+    super(points);
+    // TODO modify as needed
+  }
+
+  public SlopedModel(float[] points, float x, float y) {
+    super(points, x, y);
+    // TODO modify as needed
+  }
 
   // TODO P4 complete this class how you see fit
   /*
@@ -15,13 +25,6 @@ public class SlopedModel extends BoxObstacle {
   Also you will need to add some of your own JSON parsing code and JSON properties for the sloped
   platform.
    */
-  public SlopedModel(float width, float height) {
-    super(width, height);
-  }
-
-  public SlopedModel(float x, float y, float width, float height) {
-    super(x, y, width, height);
-  }
 
 
   public void initialize(AssetDirectory directory, JsonValue json) {
