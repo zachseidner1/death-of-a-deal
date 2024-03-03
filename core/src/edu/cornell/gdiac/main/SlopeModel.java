8f8bcee9ad0f687aab17a2ce45aa77b4a44a00f9
Vincent Jiang <vj68@cornell.edu>
2024-03-03 13:29:28 -0500

    Clean P4 TODO comments

--- core/src/edu/cornell/gdiac/main/SlopeModel.java
+++ core/src/edu/cornell/gdiac/main/SlopeModel.java
@@ -12,16 +12,6 @@ public class SlopeModel extends PolygonObstacle {
     super(new float[] {0, 0, 1, 0, 1, 1, 0, 1}, 0, 0);
   }
 
-  // TODO P4 complete this class how you see fit
-  /*
-  This should be a customizable slope, so it would be good if we could have 3 vertices and then
-  a platform would be created based off of that. I think you could try using BoxObstacle to
-  implement this, but if you find a better way feel free to do so.
-
-  Also you will need to add some of your own JSON parsing code and JSON properties for the sloped
-  platform.
-   */
-
   /**
    * Initializes the sloped platform via the given JSON value
    *
