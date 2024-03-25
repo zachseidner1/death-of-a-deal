43f5645fd50fd533e894e24ab7e9907f81279c68
zachseidner1 <zachary.seidner@gmail.com>
2024-03-25 11:13:43 -0400

    Perform a very basic merge conflict resolution

--- core/src/edu/cornell/gdiac/util/TiledJsonParser.java
+++ core/src/edu/cornell/gdiac/util/TiledJsonParser.java
@@ -140,7 +140,7 @@ public class TiledJsonParser {
       case "player":
         assert (obstacle instanceof PlayerModel);
         PlayerModel player = (PlayerModel) obstacle;
-        player.initialize(directory, json);
+        player.initialize(directory, json, tHeight);
         break;
       case "slope":
         assert (obstacle instanceof SlopeModel);
